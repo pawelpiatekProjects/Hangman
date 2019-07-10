575bfa0f0c3e536fb9401708db1a7db892be0c2f
Pawel Piatek <pawelpiatek98@gmail.com>
2019-07-10 11:03:27 +0200

    added styles to head and core:w

--- src/Hangman/Body/style.js
+++ src/Hangman/Body/style.js
@@ -1,15 +1,40 @@
 import styled from 'styled-components';
 
 export const BodyWrapper = styled.div`
+background-color: #eee;
+
+height: 100rem;
+justify-self: center;
+margin: 3rem;
+padding: 3rem;
+position: relative;
 
 `;
 
 export const Head = styled.div`
-
+  width: 10rem;
+  height: 10rem;
+  background-color: orangered;
+  display: inline-block;
+  text-align: center;
+  border-radius: 50%;
+  position: absolute;
+  top: 3rem;
+  right: 50%;
+  transform: translateX(50%);
+  margin: 0 auto;
 `;
 
 export const Core = styled.div`
-
+  width: 5rem;
+  height: 15rem;
+  background-color: brown;
+  display: inline-block;
+  text-align: center;
+  position: absolute;
+  top: 13rem;
+  right: 50%;
+  transform: translateX(50%);
 `;
 
 export const Hand = styled.div`
